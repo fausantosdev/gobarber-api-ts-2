ed9d0c7db6166f630e422f31c45485df70be0429
fallsantos <fallsantosdev@hotmail.com>
2022-04-11 10:08:25 -0300

    :construction: add schedule service

---
+++ src/app/services/CreateAppointmentService.ts
@@ -0,0 +1,36 @@
+import {startOfHour} from 'date-fns'
+import {getCustomRepository} from 'typeorm'
+import Appointment from '../models/Appointment'
+import AppointmentsRepository from '../repositories/AppointmentsRepository'
+
+interface Request {
+  provider: string
+  date: Date
+}
+
+class CreateAppointmentService {
+  public async execute({ date, provider }: Request): Promise<Appointment> {
+    const appointmentsReporitory = getCustomRepository(AppointmentsRepository)
+
+    const appointmentDate = startOfHour(date)
+
+    const findAppointmentInSameDate = await appointmentsReporitory.findByDate(
+      appointmentDate
+    )
+
+    if(findAppointmentInSameDate){
+      throw Error('This appointment is already booked')
+    }
+
+    const appointment = appointmentsReporitory.create({
+      provider,
+      date: appointmentDate
+    })
+
+    await appointmentsReporitory.save(appointment)
+
+    return appointment
+  }
+}
+
+export default CreateAppointmentService
