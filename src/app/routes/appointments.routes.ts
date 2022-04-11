d3826f6035358366219a6694a1f5ce36b2d25a92
fallsantos <fallsantosdev@hotmail.com>
2022-04-11 10:06:24 -0300

    :construction: add schedule routes 2

---
+++ src/app/routes/appointments.routes.ts
@@ -0,0 +1,48 @@
+import { Router, Request, Response } from 'express'
+import { parse, parseISO } from 'date-fns'
+import { getCustomRepository } from 'typeorm'
+
+import AppointmentRepository from '../repositories/AppointmentsRepository'
+import CreateAppointmentService from '../services/CreateAppointmentService'
+
+const router = Router()
+
+//const appointmentRepository = new AppointmentRepository()
+
+router.get('/', async (request, response) => {
+  const appointmentRepository = getCustomRepository(AppointmentRepository)
+
+  const appointments = await appointmentRepository.find()
+
+  return response.json(appointments)
+})
+
+router.post('/', async (request, response) => {
+  try {
+    const { provider, date } = request.body
+
+    const parsedDate = parseISO(date)
+
+    const createAppointment = new CreateAppointmentService()
+
+    const appointment = await createAppointment.execute({
+      date: parsedDate,
+      provider
+    })
+
+    return response.json(appointment)
+
+  } catch (err) {
+    return response.status(400).json({ error: err.message })
+  }
+})
+
+router.put('/', (request, response) => {
+  return response.json({ a: 'update'})
+})
+
+router.delete('/', (request, response) => {
+  return response.json({ a: 'delete' })
+})
+
+export default router
