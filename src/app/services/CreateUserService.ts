d8126e98c4f2c437a3050b730faea14fa57eedb0
fallsantos <fallsantosdev@hotmail.com>
2022-04-12 14:24:19 -0300

    :construction: create user service

---
+++ src/app/services/CreateUserService.ts
@@ -0,0 +1,35 @@
+import { getRepository } from 'typeorm'
+
+import User from '../models/User'
+
+interface Request {
+  name: string
+  email: string
+  password: string
+}
+
+class CreateUserService {
+ async execute({name, email, password}: Request): Promise<User> {
+   // Repository não é necessário se não terá nenhum método personalizado, ou seja, que não seja nativo do typeorm.
+    const usersRepository = getRepository(User)
+
+    const userExists = await usersRepository.findOne({
+      where: { email }
+    })
+
+    if(userExists){
+      throw new Error('Email address already user')
+    }
+
+    // Cria a intância, não salva.
+    const user = usersRepository.create({
+      name, email, password
+    })
+
+    await usersRepository.save(user)
+
+    return user
+  }
+}
+
+export default CreateUserService
