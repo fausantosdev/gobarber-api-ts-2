39c2e6984bfd8833a4392aa659d8cc63207841c6
fallsantos <fallsantosdev@hotmail.com>
2022-04-12 14:34:29 -0300

    :construction: add user routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,7 +3,9 @@ import { Router } from 'express';
 const routes = Router();
 
 import appointmentRoutes from './appointments.routes'
+import userRoutes from './users.routes'
 
+routes.use('/users', userRoutes)
 routes.use('/appointments', appointmentRoutes)
 
 export default routes
---
+++ src/app/routes/users.routes.ts
@@ -0,0 +1,47 @@
+import { Router, Request, Response } from 'express'
+import CreateUserService from '../services/CreateUserService'
+import { getRepository } from 'typeorm'
+
+import User from '../models/User'
+
+const router = Router()
+
+router.get('/', async (request, response) => {
+
+  const usersRepository = getRepository(User)
+
+  try {
+    const users = await usersRepository.find({})
+
+    return response.json(users)
+  } catch (err: any) {
+    return response.status(400).json({ error: err.message })
+  }
+})
+
+router.post('/', async (request, response) => {
+  const {name, email, password} = request.body
+
+  const createUser = new CreateUserService()
+
+  try {
+    const user = await createUser.execute({
+      name, email, password
+    })
+
+    return response.json(user)
+
+  } catch (err: any) {
+    return response.status(400).json({ error: err.message })
+  }
+})
+
+router.put('/', (request, response) => {
+  return response.json({ a: 'update'})
+})
+
+router.delete('/', (request, response) => {
+  return response.json({ a: 'delete' })
+})
+
+export default router
