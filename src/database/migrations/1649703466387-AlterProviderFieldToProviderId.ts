59c85041e81933a14283506af9a512cafb69fbd5
fallsantos <fallsantosdev@hotmail.com>
2022-04-12 14:22:48 -0300

    :construction: add foreign key referring to users table

---
+++ src/database/migrations/1649703466387-AlterProviderFieldToProviderId.ts
@@ -0,0 +1,36 @@
+import {MigrationInterface, QueryRunner, TableColumn, TableForeignKey} from "typeorm";
+
+export class AlterProviderFieldToProviderId1649703466387 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropColumn('appointments', 'provider')
+
+      await queryRunner.addColumn('appointments', new TableColumn({
+        name: 'provider_id',
+        type: 'uuid',
+        isNullable: true
+      }))
+
+      await queryRunner.createForeignKey('appointments', new TableForeignKey({
+        name: 'rk_appointment_provider',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',// RESTRICT, CASCADE...
+        onUpdate: 'CASCADE'
+      }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropForeignKey('appointments','rk_appointment_provider')
+
+      await queryRunner.dropColumn('appointments', 'provider_id')
+
+      await queryRunner.addColumn('appointments', new TableColumn({
+        name: 'provider',
+        type: 'varchar',
+        isNullable: false,
+      }))
+    }
+
+}
