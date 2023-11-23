cb4ab8b4f481b22093cfd841a8baf93b463c35bf
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-11-22 21:11:01 -0300

    :construction: fix class name

--- src/modules/users/repositories/Fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/Fakes/FakeUsersRepository.ts
@@ -5,7 +5,7 @@ import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO'
 
 import User from '../../infra/typeorm/entities/User'
 
-class UsersRepository implements IUserRepository {
+class FakeUsersRepository implements IUserRepository {
   private users: User[] = []
 
   public async create({name,email,password}: ICreateUserDTO): Promise<User> {
@@ -39,4 +39,4 @@ class UsersRepository implements IUserRepository {
   }
 }
 
-export default UsersRepository
+export default FakeUsersRepository
