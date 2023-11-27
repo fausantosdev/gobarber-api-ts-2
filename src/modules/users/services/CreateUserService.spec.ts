f18522a00cd14b68971ec1bae004fedd089b1d8e
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-11-27 09:47:56 -0300

    :construction: separates the responsibility of dealing with passwords

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,4 +1,6 @@
 import FakeUsersRepository from '../repositories/Fakes/FakeUsersRepository'
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider'
+
 import CreateUserService from './CreateUserService'
 
 import AppError from '@shared/errors/AppError'
@@ -6,8 +8,12 @@ import AppError from '@shared/errors/AppError'
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new FakeUsersRepository()
+    const fakeHashProvider = new FakeHashProvider()
 
-    const createUser = new CreateUserService(fakeUsersRepository)
+    const createUser = new CreateUserService(
+        fakeUsersRepository,
+        fakeHashProvider
+      )
 
     const user = await createUser.execute({
       name: 'John Doe',
@@ -20,8 +26,12 @@ describe('CreateUser', () => {
 
   it('should not be able to create a new user with same email from another', async () => {
     const fakeUsersRepository = new FakeUsersRepository()
+    const fakeHashProvider = new FakeHashProvider()
 
-    const createUser = new CreateUserService(fakeUsersRepository)
+    const createUser = new CreateUserService(
+        fakeUsersRepository,
+        fakeHashProvider
+      )
 
     await createUser.execute({
       name: 'John Doe',
