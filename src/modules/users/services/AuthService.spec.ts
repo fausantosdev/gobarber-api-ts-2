392ede22037e72024ee0ffccb425ba12c1afc2e4
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-12-07 11:53:18 -0300

    :construction: testing authentication

--- src/modules/users/services/AuthService.spec.ts
+++ src/modules/users/services/AuthService.spec.ts
@@ -32,4 +32,40 @@ describe('AuthUser', () => {
     expect(response).toHaveProperty('token')
     expect(response.user).toEqual(user)
   })
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeHashProvider = new FakeHashProvider()
+
+    const authUser = new AuthService(fakeUsersRepository, fakeHashProvider)
+
+    expect(authUser.execute({
+      email: 'johndoe@mail.com',
+      password: '1234abcd'
+    })).rejects.toBeInstanceOf(AppError)
+
+  })
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeHashProvider = new FakeHashProvider()
+
+    const createUser = new CreateUserService(
+        fakeUsersRepository,
+        fakeHashProvider
+      )
+
+    const authUser = new AuthService(fakeUsersRepository, fakeHashProvider)
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      password: '1234abcd'
+    })
+
+    expect(authUser.execute({
+      email: 'johndoe@mail.com',
+      password: 'abcd1234'
+    })).rejects.toBeInstanceOf(AppError)
+  })
 })
