ffc839265695633b3658d66d7a1182359d749f5b
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-12-27 11:26:54 -0300

    :white_check_mark: ensures that the password hash function was called correctly

--- src/modules/users/services/ResetPasswordService.spec.ts
+++ src/modules/users/services/ResetPasswordService.spec.ts
@@ -1,5 +1,6 @@
 import FakeUsersRepository from '../repositories/Fakes/FakeUsersRepository'
 import FakeUserTokensRepository from '../repositories/Fakes/FakeUserTokensRepository'
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider'
 
 import ResetPasswordService from './ResetPasswordService'
 import AppError from '@shared/errors/AppError'
@@ -7,15 +8,18 @@ import AppError from '@shared/errors/AppError'
 let fakeUsersRepository: FakeUsersRepository
 let fakeUserTokensRepository: FakeUserTokensRepository
 let resetPasswordService: ResetPasswordService
+let fakeHashProvider: FakeHashProvider
 
-describe('SendForgotPasswordEmail', () => {
+describe('ResetPasswordService', () => {
   beforeEach(() => {// beforeEach: antes de cada teste, beforeAll: antes de todos os testes
     fakeUsersRepository = new FakeUsersRepository()
     fakeUserTokensRepository = new FakeUserTokensRepository()
+    fakeHashProvider = new FakeHashProvider
 
     resetPasswordService = new ResetPasswordService(
         fakeUsersRepository,
-        fakeUserTokensRepository
+        fakeUserTokensRepository,
+        fakeHashProvider
     )
   })
 
@@ -28,6 +32,8 @@ describe('SendForgotPasswordEmail', () => {
 
     const { token } = await fakeUserTokensRepository.generate(user.id)
 
+    const generateHash = jest.spyOn(fakeHashProvider, 'generateHash')
+
     await resetPasswordService.execute({
       token,
       password: '654321'
@@ -35,6 +41,7 @@ describe('SendForgotPasswordEmail', () => {
 
     const updatedUser = await fakeUsersRepository.findById(user.id)
 
+    expect(generateHash).toHaveBeenCalledWith('654321')
     expect(updatedUser?.password).toBe('654321')
   })
 })
