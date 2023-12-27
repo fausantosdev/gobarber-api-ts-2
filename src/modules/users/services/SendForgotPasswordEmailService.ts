8004c061ad32b5e7e148e2b3253931a436229425
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-12-27 10:07:16 -0300

    :construction: should not be able to recover a non-existing user password and generate a forgot password token

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -3,6 +3,10 @@ import { injectable, inject } from 'tsyringe'
 import IUsersRepository from '../repositories/IUsersRepository'
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider'
 
+import IUserTokensRepository from '../repositories/IUserTokensRepository'
+
+import AppError from '@shared/errors/AppError'
+
 interface IRequest {
   email: string
 }
@@ -14,10 +18,21 @@ class SendForgotPasswordEmailService {
     private usersRepository: IUsersRepository,
 
     @inject('MailProvider')
-    private mailProvider: IMailProvider
+    private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if(!user) {
+      throw new AppError('User does not exists.', 400)
+    }
+
+    await this.userTokensRepository.generate(user.id)
+
     this.mailProvider.sendMail(email, 'Pedido de recuperação de senha recebido.')
   }
 }
