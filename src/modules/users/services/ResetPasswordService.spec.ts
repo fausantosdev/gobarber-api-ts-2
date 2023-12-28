2de5d6facb16cacace986992a7ad9d61224c4e6f
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-12-28 11:01:29 -0300

    :white_check_mark: just changes the order of tests

--- src/modules/users/services/ResetPasswordService.spec.ts
+++ src/modules/users/services/ResetPasswordService.spec.ts
@@ -23,28 +23,6 @@ describe('ResetPasswordService', () => {
     )
   })
 
-  it('should be able to reset password', async () => {
-    const user = await fakeUsersRepository.create({
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      password: '123456'
-    })
-
-    const { token } = await fakeUserTokensRepository.generate(user.id)
-
-    const generateHash = jest.spyOn(fakeHashProvider, 'generateHash')
-
-    await resetPasswordService.execute({
-      token,
-      password: '654321'
-    })
-
-    const updatedUser = await fakeUsersRepository.findById(user.id)
-
-    expect(generateHash).toHaveBeenCalledWith('654321')
-    expect(updatedUser?.password).toBe('654321')
-  })
-
   it('should not be able to reset the password with non-existing token', async () => {
     await expect(
       resetPasswordService.execute({
@@ -86,5 +64,27 @@ describe('ResetPasswordService', () => {
         password: '123456'
       })
     ).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('should be able to reset password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456'
+    })
+
+    const { token } = await fakeUserTokensRepository.generate(user.id)
+
+    const generateHash = jest.spyOn(fakeHashProvider, 'generateHash')
+
+    await resetPasswordService.execute({
+      token,
+      password: '654321'
+    })
+
+    const updatedUser = await fakeUsersRepository.findById(user.id)
+
+    expect(generateHash).toHaveBeenCalledWith('654321')
+    expect(updatedUser?.password).toBe('654321')
   })
 })
