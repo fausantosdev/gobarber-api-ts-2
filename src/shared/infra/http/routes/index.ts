a7ee5a1a268a43d1154bdffd7765a464b3196e15
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-12-28 10:49:55 -0300

    :construction: create password routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express'
 import sessionRoutes from '@modules/users/infra/http/routes/sessions.routes'
 import userRoutes from '@modules/users/infra/http/routes/users.routes'
 import appointmentRoutes from '@modules/appointments/infra/http/routes/appointments.routes'
+import passwordRoutes from '@modules/users/infra/http/routes/password.routes'
 
 const routes = Router()
 
 routes.use('/auth', sessionRoutes)
 routes.use('/users', userRoutes)
 routes.use('/appointments', appointmentRoutes)
+routes.use('/password', passwordRoutes)
 
 export default routes
