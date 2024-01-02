29b503cd89da55449e8b9e64af72169d2aeed163
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-01-01 22:37:54 -0300

    :construction: retrieves the token and sends it along with the email message

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,7 +1,7 @@
 import { injectable, inject } from 'tsyringe'
 
 import IUsersRepository from '../repositories/IUsersRepository'
-import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider'
+import { IMailProvider } from '@shared/container/providers/MailProvider/models/IMailProvider'
 
 import IUserTokensRepository from '../repositories/IUserTokensRepository'
 
@@ -25,15 +25,16 @@ class SendForgotPasswordEmailService {
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
+
     const user = await this.usersRepository.findByEmail(email)
 
     if(!user) {
       throw new AppError('User does not exists.', 400)
     }
 
-    await this.userTokensRepository.generate(user.id)
+    const { token } = await this.userTokensRepository.generate(user.id)
 
-    this.mailProvider.sendMail(email, 'Pedido de recuperação de senha recebido.')
+    await this.mailProvider.sendMail(email, `Pedido de recuperação de senha recebido: ${token}`)
   }
 }
 
