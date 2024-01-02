2233f7fe793c9a50204071f08e642027f00fc8c8
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-01-01 22:44:29 -0300

    :construction: fix date comparison

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -1,13 +1,12 @@
 import { injectable, inject } from 'tsyringe'
-import { differenceInHours, isAfter, addHours } from 'date-fns'
+import { isAfter, addHours } from 'date-fns'
 
 import IUsersRepository from '../repositories/IUsersRepository'
-
 import IUserTokensRepository from '../repositories/IUserTokensRepository'
 
-import AppError from '@shared/errors/AppError'
-
 import IHashProvider from '../providers/HashProvider/models/IHashProvider'
+
+import AppError from '@shared/errors/AppError'
 
 interface IRequest {
   token: string
@@ -28,25 +27,23 @@ class ResetPasswordService {
   ) {}
 
   public async execute({ token, password }: IRequest): Promise<void> {
-      const userToken = await this.userTokensRepository.findByToken(token)
+    const userToken = await this.userTokensRepository.findByToken(token)
 
-      if (!userToken) throw new AppError('User token does not exists', 401)
+    if (!userToken) throw new AppError('User token does not exists', 401)
 
-      const user = await this.usersRepository.findById(userToken.user_id)
+    const user = await this.usersRepository.findById(userToken.user_id)
 
-      if (!user) throw new AppError('User does not exists', 401)
+    if (!user) throw new AppError('User does not exists', 401)
 
-      const tokenCreatedAt = userToken.created_at
+    const tokenCreatedAt = userToken.created_at
 
-      //console.log(differenceInHours(Date.now(), tokenCreatedAt))
-      //if (differenceInHours(Date.now(), tokenCreatedAt) > 2) throw new AppError('Token expired', 401)
-      const compareDate = addHours(tokenCreatedAt, 2)
+    const compareDate = addHours(tokenCreatedAt, 2)
 
-      if (isAfter(Date.now(), compareDate)) throw new AppError('Token expired', 401)
+    if (isAfter(Date.now(), compareDate)) throw new AppError('Token expired', 401)
 
-      user.password = await this.hashProvider.generateHash(password)
+    user.password = await this.hashProvider.generateHash(password)
 
-      await this.usersRepository.save(user)
+    await this.usersRepository.save(user)
   }
 }
 
