e72a4254867fbbd1ae35465072315aa8c57de0c1
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-01-18 15:02:04 -0300

    :construction: configure template engine

--- src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -2,8 +2,8 @@ import IParceMailTemplateDTO from '../dtos/IParceMailTemplateDTO'
 import IMailTemplateProvider from '../models/IMailTemplateProvider'
 
 class FakeMailTemplateProvider implements IMailTemplateProvider {
-  public async parse({ template, variables }: IParceMailTemplateDTO): Promise<string> {
-    return template
+  public async parse({ file, variables }: IParceMailTemplateDTO): Promise<string> {
+    return 'Mail content'
   }
 }
 
